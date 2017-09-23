6bee66dedd45725729c4cd53c23539efce6ddaee
Kenneth Endfinger <kaendfinger@gmail.com>
2017-09-23 19:53:59 -0400

    Add Android Toolchain

---
+++ cmake/android-toolchain.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Android)
+set(CMAKE_SYSTEM_VERSION 18)
+set(CMAKE_ANDROID_ARCH_ABI armeabi-v7a)
+
+if (NOT DEFINED CMAKE_ANDROID_NDK)
+  set(CMAKE_ANDROID_NDK $ENV{HOME}/Android/Sdk/ndk-bundle)
+endif()
+
+set(CMAKE_ANDROID_STL_TYPE gnustl_static)
+
